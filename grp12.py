2f3995ef18ac90048b2183a1216a7a7db9b9324a
whoami2me <87486339+whoami2me@users.noreply.github.com>
2021-08-14 21:46:35 +0800

    Update grp12.py

--- grp12.py
+++ grp12.py
@@ -22,7 +22,7 @@ url2 = input("Please enter url2:")
 modified_ua = requests.get(url2, headers=headers)
 print(modified_ua.text)
 
-print("end of part 5")
+print("end of part 5 \n**************************")
 
 
 #part6 
