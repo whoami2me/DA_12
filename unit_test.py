6408661d749466da28fcbb52670709f8ab573614
whoami2me <87486339+whoami2me@users.noreply.github.com>
2021-08-19 02:41:29 +0800

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -1,2 +1,31 @@
 #part8
+import scrapy
+from scrapy.http import HtmlResponse
+import grp12
 import unittest
+from betamax import Betamax
+from betamax.fixtures.unittest import BetamaxTestCase
+
+with Betamax.configure() as config:
+    config.cassette_library_dir = 'cassettes'
+    config.preserve_exact_body_bytes = True
+    
+class TestExample(BetamaxTestCase):
+    
+    def test_parse(self):
+        new_spider = grp12.New_Spider()
+        
+        response = self.session.get(new_spider.start_urls)
+        
+        scrapy_response = HtmlResponse(body=response.content,url=new_spider.start_urls)
+        
+        result = new_spider.parse(scrapy_response)
+        
+        self.assertEqual({'Image Link': u'image1.img'}, result.next())
+        self.assertEqual({'Image Link': u'image2.img'}, result.next())
+        self.assertEqual({'Image Link': u'image3.img'}, result.next())
+        self.assertEqual({'Image Link': u'image4.img'}, result.next())
+        self.assertEqual({'Image Link': u'image5.img'}, result.next())
+        
+        with self.assertRaises(StopIteration):
+            result.next()
