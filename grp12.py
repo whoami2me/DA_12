cde678262ad190cbe557d8ede6c33e1633e90e8c
whoami2me <87486339+whoami2me@users.noreply.github.com>
2021-08-19 10:37:11 +0800

    Update grp12.py

--- grp12.py
+++ grp12.py
@@ -1,6 +1,7 @@
 import requests
 import scrapy
 
+# need function 
 url = input("Please enter url:")
 r = requests.get(url)
 
@@ -31,19 +32,19 @@ class NewSpider(scrapy.Spider):
  def parse(self,response):
   css_selector = 'img'
   for x in response.css(css_selector):
-   newsel = '@src'
-   yield{
-    'Image Link' : x.xpath(newsel).extract_first(),
-   }
-   Page_selector = '.next a ::attr(href)'
-   next_page = response.css(Page_selector).extract_first()
-   if next_page:
-    yield scrapy.Request(
-      response.urljoin(next_page),
-      callback=self.parse
-    )
+       url = x.get()
+       if any(extension in url for extension in ['jpg']):
+       newsel = '@src'
+       yield{
+         'Image Link' : x.xpath(newsel).extract_first(),
+       }
+        
+       Page_selector = '.next a ::attr(href)'
+       next_page = response.css(Page_selector).extract_first()
+       if next_page:
+         yield scrapy.Request(
+         response.urljoin(next_page),
+         callback=self.parse
+         )
 
-    url = x.get()
-        if any(extension in url for extension in ['jpg']):
-            newsel = '@src'
-#missing 6ii and 6iii 
+#all done 
