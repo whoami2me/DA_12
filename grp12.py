bd62ed667869c006d5fdfd25c34ac2a87be78790
whoami2me <ldonen90@gmail.com>
2021-08-26 12:52:45 +0800

    updated

--- grp12.py
+++ grp12.py
@@ -1,13 +1,15 @@
 import requests
 import scrapy
+import unittest
+from unittest import TestCase
+import json
 
 url = 'https://brickset.com/sets/year-2009'
 req = requests.get(url)
-print(req.text)
 
 print("Status code:")
 print("\t *", req.status_code)
-
+x=input("smth")
 h = requests.head(url)
 print("Header:")
 print("**********")
@@ -15,14 +17,14 @@ print("**********")
 for line in h.headers:
     print("\t", line, ":", h.headers[line])
 print("**********")
-
+y=input("smmth")
 headers = {
     'User-Agent': 'Mobile'
 }
 modified_ua = requests.get(url, headers=headers)
 print(modified_ua.request.headers)
 print("**************")
-
+z = input("smth")
 class BrickSetSpider(scrapy.Spider):
     name = "brickset_spider"
     start_urls = ['https://brickset.com/sets/year-2009']
@@ -45,3 +47,18 @@ class BrickSetSpider(scrapy.Spider):
           callback=self.parse
           )
 
+class test_part5(TestCase):
+    def test_sc(self):
+        response = requests.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def mua(self):
+        responseua = modified_ua
+        self.assertEqual(responseua.request.headers, {'User-Agent': 'Mobile', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive'})
+
+class test_file(TestCase):
+    def test_json(self):
+        f = open("resultss.json",)
+        data_jpg = json.loads(f.read())
+        self.assertEqual(data_jpg[1], {'Image Link': 'https://images.brickset.com/sets/small/3594-1.jpg?200811140307'})
+        f.close()
