93c1d556494d528c5ddf006525e25c649592df13
whoami2me <ldonen90@gmail.com>
2021-08-26 22:42:26 +0800

    updated files

--- grp12.py
+++ grp12.py
@@ -1,6 +1,5 @@
 import requests
 import scrapy
-import unittest
 from unittest import TestCase
 import json
 
@@ -9,7 +8,7 @@ req = requests.get(url)
 
 print("Status code:")
 print("\t *", req.status_code)
-x=input("smth")
+
 h = requests.head(url)
 print("Header:")
 print("**********")
@@ -17,14 +16,15 @@ print("**********")
 for line in h.headers:
     print("\t", line, ":", h.headers[line])
 print("**********")
-y=input("smmth")
+
 headers = {
     'User-Agent': 'Mobile'
 }
 modified_ua = requests.get(url, headers=headers)
 print(modified_ua.request.headers)
 print("**************")
-z = input("smth")
+
+
 class BrickSetSpider(scrapy.Spider):
     name = "brickset_spider"
     start_urls = ['https://brickset.com/sets/year-2009']
@@ -47,18 +47,21 @@ class BrickSetSpider(scrapy.Spider):
           callback=self.parse
           )
 
-class test_part5(TestCase):
+class test_part(TestCase):
+
     def test_sc(self):
-        response = requests.get(url)
+        sample_url = 'https://brickset.com/sets/year-2009'
+        response = requests.get(sample_url)
         self.assertEqual(response.status_code, 200)
 
     def mua(self):
-        responseua = modified_ua
-        self.assertEqual(responseua.request.headers, {'User-Agent': 'Mobile', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive'})
+        sample_url2 = 'https://brickset.com/sets/year-2009'
+        headers = {'User-Agent': 'Mobile'}
+        responseua = requests.get(sample_url2, headers=headers)
+        self.assertEqual(responseua.request.headers, 'Mobile')
 
-class test_file(TestCase):
     def test_json(self):
         f = open("resultss.json",)
         data_jpg = json.loads(f.read())
-        self.assertEqual(data_jpg[1], {'Image Link': 'https://images.brickset.com/sets/small/3594-1.jpg?200811140307'})
+        self.assertEqual(data_jpg[0], {"Image Link": "https://images.brickset.com/sets/small/2009-1.jpg?201012241125"})
         f.close()
